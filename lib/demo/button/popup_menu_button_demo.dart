1b9b46f337c781ceb8f2693804f92390a8fee643
zulu chen <chenzulu@163.com>
2020-03-22 21:36:37 +0800

    新增PopupMenuButtonDemo下拉菜单

--- lib/demo/button/popup_menu_button_demo.dart
+++ lib/demo/button/popup_menu_button_demo.dart
@@ -8,6 +8,7 @@ class PopupMenuButtonDemo extends StatefulWidget {
 }
 
 class _PopupMenuButtonDemoState extends State<PopupMenuButtonDemo> {
+  String _currentMenuItem = 'Home';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +22,28 @@ class _PopupMenuButtonDemoState extends State<PopupMenuButtonDemo> {
         padding: EdgeInsets.all(16),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(_currentMenuItem),
+                PopupMenuButton(
+                  onSelected: (value) {
+                    // 设置选中的值
+                    setState(() {
+                      _currentMenuItem = value;
+                    });
+                    print(value);
+                  },
+                  itemBuilder: (BuildContext context) => [
+                    PopupMenuItem(value: 'Home', child: Text('Home')),
+                    PopupMenuItem(value: 'Discover', child: Text('Discover')),
+                    PopupMenuItem(value: 'Community', child: Text('Community'))
+                  ],
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
